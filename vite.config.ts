ad503c81de052aac2824b53732493b1da96ca665
Benoit Mariat <benoit.mariat@intersec.com>
2025-07-01 19:20:31 +0200

    fix deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import pkg from './package.json';
 
-// https://vite.dev/config/
+/* https://vite.dev/config/ */
 export default defineConfig({
     plugins: [
         vue(),
